47a39b61a92146783862bbc4ad31a48ec4b1fc89
carter-oleary <carter.m.oleary@outlook.com>
2025-07-10 18:25:57 -0400

    bugfixes

--- GolfCompanion/Services/Converters.cs
+++ GolfCompanion/Services/Converters.cs
@@ -25,7 +25,7 @@ namespace GolfCompanion
 
     public class StringToBoolConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
             if (value is string stringValue)
             {
@@ -34,7 +34,7 @@ namespace GolfCompanion
             return false;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
